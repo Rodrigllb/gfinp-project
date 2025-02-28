fa6da3cf8b8d41963511880cdb42a7ac757dcd72
rdrig <rdrigoll7@gmail.com>
2025-02-28 12:21:20 -0300

    Recomeço

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,14 @@
 <template>
-  <appHeader></appHeader>
-  <appMain></appMain>
-  <appFooter></appFooter>
+  
 </template>
 
 <script>
-import appHeader from './components/appHeader.vue';
-import appMain from './components/appMain.vue';
-import appFooter from './components/appFooter.vue';
+
 
 export default {
   name: 'App',
   components: {
-    appHeader,
-    appMain,
-    appFooter,
+      
   }
 } 
 </script>
